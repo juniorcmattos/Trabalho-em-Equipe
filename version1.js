35c9cb3d048d173cb5a9e4976bf13c10fe5ea023
DiegoLions <diegohsilva-2010@hotmail.com>
2025-07-22 11:31:58 -0300

    Inclusão do listar

--- version1.js
+++ version1.js
@@ -1,67 +1,11 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin
+    input: process.stdin,
     output: process.stdout
 });
 
 let lembretes = [];
-
-
-function menuprincipal () {
-    console.log ('1. Adicionar lembrete');
-    console.log ('2. Listar lembretes');
-    console.log ('3. Editar lembrete');
-    console.log ('4. Marcar lembrete como concluído');
-    console.log ('5. Sair');
-}
-
-function mostrarMenu () {
-    console.log('\n' + '='.repeat(30));
-    menuprincipal();
-    console.log('='.repeat(30));
-}
-
-rl.question('Escolha uma opção: ',(opcao) =>  {
-    switch (opcao) {
-        case '1':
-            AdicionarLembrete();
-            break;
-        case '2':
-            ListarLembretes();
-            break;
-        case '3':
-            EditarLembrete();
-            break;
-        case '4':
-            MarcarLembreteComoConcluido();
-            break;
-        case '5':
-        rl.close();
-        break;
-    default:
-        console.log('Opção inválida. Tente novamente.');
-        mostrarMenu();
-    }
-});
-}
-
-function ListarLembretes() {
-    if (lembretes.length === 0) {
-        console.log('Nenhum lembrete adicionado.');
-    } else {
-        console.log('\n=== LISTA DE LEMBRETES ===');
-        lembretes.forEach((lembrete, index) => {
-            console.log(
-                `${index + 1}. Nome: ${lembrete.nome} | Prazo: ${lembrete.prazo} | Concluído: ${LembreteConcluído.resposta}`
-            );
-        });
-    }
-
-console.log('\n Pressione Enter para voltar ao menu principal.');
-    rl.question('', mostrarMenu);
-    }
-}
 
 // --- Funções de Lembrete ---
 
@@ -80,7 +24,7 @@ function menu() {
               criarLembrete();
               break;
           case '2':
-              listar();
+              ListarLembretes();
               break;
           case '3':
               editar();
@@ -119,6 +63,24 @@ function criarLembrete() {
       });
   });
 }
+
+// Função listar lembrete
+
+function ListarLembretes() {
+  if (lembretes.length === 0) {
+      console.log('Nenhum lembrete adicionado.');
+  } else {
+      console.log('\n=== LISTA DE LEMBRETES ===');
+      lembretes.forEach((lembrete, index) => {
+          console.log(
+              `${index + 1}. Nome: ${lembrete.nome} | Prazo: ${lembrete.prazo} | Concluído: ${LembreteConcluído.resposta}`
+          );
+      });
+  }
+
+console.log('\n Pressione Enter para voltar ao menu principal.');
+  rl.question('', mostrarMenu);
+  }
 
 function concluirTarefa() {
     if (lista.length === 0) {
